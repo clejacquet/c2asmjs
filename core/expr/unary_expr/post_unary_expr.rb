4bedcd2494fe01fedf6da4287b532991057a64b6
CyrilBos <bos.cyril33@gmail.com>
2016-12-28 19:47:37 +0100

    merge correction

--- core/expr/unary_expr/post_unary_expr.rb
+++ core/expr/unary_expr/post_unary_expr.rb
@@ -2,14 +2,15 @@ require_relative('unary_expr')
 
 class PostUnaryExpr < UnaryExpr
   def code(scope)
-    expr_code, expr_reg, expr_type = @expr.code(scope)
+    expr_type = @expr.type(scope)
+    expr_code, expr_reg = @expr.code(scope)
     reg = scope.new_register
     return "#{expr_code}#{reg} = #{Type.to_llvm_op(sym, expr_type)} #{Type.to_llvm(expr_type)} #{val(expr_type)}, #{expr_reg}\n",
-        reg,#expr_reg ?
+        reg,
         expr_type
   end
 
   def try_eval
-    - @expr.try_eval
+    @expr.try_eval
   end
 end
--- core/expr/unary_expr/unary_expr.rb
+++ core/expr/unary_expr/unary_expr.rb
@@ -12,14 +12,18 @@ class UnaryExpr
   end
 
   def code(scope)
-    expr_code, expr_reg, expr_type = @expr.code(scope)
+    expr_type = @expr.type(scope)
+    expr_code, expr_reg = @expr.code(scope)
     reg = scope.new_register
     return "#{expr_code}#{reg} = #{Type.to_llvm_op(sym, expr_type)} #{Type.to_llvm(expr_type)} #{val(expr_type)}, #{expr_reg}\n",
-        reg,#reg ?
-        expr_type
+        reg
+  end
+
+  def type(scope)
+    @expr.type(scope)
   end
 
   def try_eval
-    - @expr.try_eval
+    @expr.try_eval
   end
 end
