eae8b63fa7b7e963c5fcb4fcabec508f1f5c7bcb
CyrilBos <bos.cyril33@gmail.com>
2016-12-28 19:39:30 +0100

    merge correction

--- core/expr/unary_expr/neg_expr.rb
+++ core/expr/unary_expr/neg_expr.rb
@@ -10,11 +10,20 @@ class NegExpr < UnaryExpr
   end
 
   def code(scope)
-    expr_code, expr_reg, expr_type = @expr.code(scope)
+    expr_type = @expr.type(scope)
+    expr_code, expr_reg = @expr.code(scope)
     reg = scope.new_register
 
     return "#{expr_code}#{reg} = #{Type.to_llvm_op('-', expr_type)} #{Type.to_llvm(expr_type)} #{val(expr_type)}, #{expr_reg}\n",
         reg,
         expr_type
+  end
+
+  def try_eval
+    - @expr.try_eval
+  end
+
+  def type(scope)
+    @expr.type(scope)
   end
 end
